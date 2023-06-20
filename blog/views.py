0343b50ba3e0ba5307002642ef49fa2ea5fe2c10
REZA <98772217+R3Z44@users.noreply.github.com>
2023-06-20 08:58:58 +0330

    Blog page created
    Blog page created. Posts from db integrated in template.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 
 
 def blog_view(request):
-    return render(request, 'blog/blog.html')
+    posts = Post.objects.all().filter(status=1)
+    context = {'posts': posts}
+    return render(request, 'blog/blog.html', context)
 
 
 def blog_single(request):
