7d001968b686af186b27a2f5c550f078fc30dae5
REZA <98772217+R3Z44@users.noreply.github.com>
2023-06-23 13:24:59 +0330

    Paginator
    Paginator has added to blog page

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.db.models.functions import Now
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
 
 
-def blog_view(request, cat_name=None):
+def blog_view(request, **kwargs):
 
     posts = Post.objects.filter(status=1)
-    if cat_name:
-        lowercase_cat_name = cat_name.lower()
-        posts = Post.objects.filter(category__name__iexact=lowercase_cat_name)
+    if kwargs.get('cat_name') != None:
+        lowercase_cat_name = kwargs['cat_name'].lower()
+        posts = posts.filter(category__name__iexact=lowercase_cat_name)
+    if kwargs.get('author_username') != None:
+        lowercase_author_username = kwargs['author_username'].lower()
+        posts = posts.filter(
+            author__username__iexact=lowercase_author_username)
+    posts = Paginator(posts, 3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts': posts}
     return render(request, 'blog/blog.html', context)
 
@@ -35,3 +48,13 @@ def blog_single(request, pid):
     post.counted_views += 1
     post.save()
     return render(request, 'blog/blog-single.html', context)
+
+
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        # request.GET.get('s')
+        posts = posts.filter(content__contains=request.GET.get(
+            's')) or posts.filter(title__contains=request.GET.get('s'))
+    context = {'posts': posts}
+    return render(request, 'blog/blog.html', context)
