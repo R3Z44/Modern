82f477945a96bc89e93284a0e3357b414c34c0b9
REZA <98772217+R3Z44@users.noreply.github.com>
2023-06-25 00:18:45 +0330

    Contact form
    Contact form created
    Popup message has been created and integrated to base.html

--- website/views.py
+++ website/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from website.models import Contact
+from website.forms import ContactForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -12,7 +16,19 @@ def about_view(request):
 
 
 def contact_view(request):
-    return render(request, 'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            new_contact = form.save()
+            new_contact.name = 'Unknown'
+            new_contact.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 'Your Message has been received successfully!!!')
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 'Your Message didn\'t received!!!')
+    form = ContactForm()
+    return render(request, 'website/contact.html', {'form': form})
 
 
 def services_view(request):
