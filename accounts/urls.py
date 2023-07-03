5971850bb6e70b73194418159e3e81040f296c16
REZA <98772217+R3Z44@users.noreply.github.com>
2023-07-04 00:27:41 +0330

    Revert "Password recovery"
    This reverts commit ba64d5227f60e0076493cdd7f66506a66f792fce.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
+
 from . import views
 from accounts.views import CustomLoginView
 
@@ -13,17 +13,4 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     # signup
     path('login/', CustomLoginView.as_view(), name='login'),
-
-    path('password_reset/', auth_views.PasswordResetView.as_view(),
-         name='reset_password'),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),
-         name='password_reset_done'),
-    path(
-        "reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
-    ),
-    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(),
-         name='password_reset_complete'),
-
 ]
